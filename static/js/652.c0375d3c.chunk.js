95587b13195ef0e1cad7aa27521214bff02547ff
Yevhenii770 <110056243+Yevhenii770@users.noreply.github.com>
2023-04-23 11:56:47 +0000

    Deploying to gh-pages from @ Yevhenii770/tweets@f05969416688ecdedb4feb7c65a8ca0f2015a005 🚀

---
+++ static/js/652.c0375d3c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktweets=self.webpackChunktweets||[]).push([[652],{652:function(e,t,n){n.r(t);var s=n(184);t.default=function(){return(0,s.jsx)("div",{children:"AMORCHENKO"})}}}]);
+//# sourceMappingURL=652.c0375d3c.chunk.js.map
