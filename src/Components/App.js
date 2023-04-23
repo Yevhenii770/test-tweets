4ed7feb9d1e818a14af71b2c61ed388e09c0f2d0
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-04-23 14:14:12 +0200

    upd name

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,19 @@
-import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-const NotFound = lazy(() => import("./Pages/NotFound/NotFound"));
-const Home = lazy(() => import("./Pages/Home/Home"));
-const Tweets = lazy(() => import("./Pages/Tweets/Tweets"));
+const NotFound = lazy(() => import('./Pages/NotFound/NotFound'));
+const Home = lazy(() => import('./Pages/Home/Home'));
+const Tweets = lazy(() => import('./Pages/Tweets/Tweets'));
 
 function App() {
   return (
-    <div>
+    <>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
